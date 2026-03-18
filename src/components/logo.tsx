dbd46ded39067ce40d36080f0e82a42c94056e13
Nani1802 <naniramirezjacinto@gmail.com>
2026-03-17 22:01:29 -0600

    mezcalero

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,26 +2,38 @@ import darkLogo from "@/assets/logos/dark.svg";
 import logo from "@/assets/logos/main.svg";
 import Image from "next/image";
 
-export function Logo() {
+export function Logo({
+  label = "Mezcalero",
+  showImage = true,
+}: {
+  label?: string;
+  showImage?: boolean;
+}) {
   return (
-    <div className="relative h-8 max-w-[10.847rem]">
-      <Image
-        src={logo}
-        fill
-        className="dark:hidden"
-        alt="NextAdmin logo"
-        role="presentation"
-        quality={100}
-      />
+    <div className="flex items-center gap-2">
+      {showImage && (
+        <div className="relative h-8 w-8">
+          <Image
+            src={logo}
+            fill
+            className="dark:hidden"
+            alt="NextAdmin logo"
+            role="presentation"
+            quality={100}
+          />
 
-      <Image
-        src={darkLogo}
-        fill
-        className="hidden dark:block"
-        alt="NextAdmin logo"
-        role="presentation"
-        quality={100}
-      />
+          <Image
+            src={darkLogo}
+            fill
+            className="hidden dark:block"
+            alt="NextAdmin logo"
+            role="presentation"
+            quality={100}
+          />
+        </div>
+      )}
+
+      <span className="text-lg font-semibold">{label}</span>
     </div>
   );
 }
